d6f191a586276ed88311e6b14699977194166adc
cantatas <zennqann@foxmail.com>
2019-11-13 14:28:58 +0800

    style css update

--- src/renderer/page/index/components/history.vue
+++ src/renderer/page/index/components/history.vue
@@ -2,18 +2,20 @@
   <div class="history-components">
       <ul class="record-list">
         <li :key="index" v-for="(key,index) in getSearchHistoryState">
-          <div class="body">
-            <div class="num">{{index+1}}.</div>
-            <div class="content">
-              <div class="query">{{key.q}}<a title="复制" @click="copyResult(key.q)" class="copy"><i class="icon iconfont iconfuzhicopy22"></i></a></div>
-              <div class="result">{{key.r}}<a title="复制" @click="copyResult(key.r)" class="copy"><i class="icon iconfont iconfuzhicopy22"></i></a></div>
-              <div class="time">{{getDayName(key.t)}} {{dateFormat(key.t)}}</div>
+          <div class="li-box">
+            <div class="body">
+              <div class="num">{{index+1}}.</div>
+              <div class="content">
+                <div class="query">{{key.q}}<a title="复制" @click="copyResult(key.q)" class="copy"><i class="icon iconfont iconfuzhicopy22"></i></a></div>
+                <div class="result">{{key.r}}<a title="复制" @click="copyResult(key.r)" class="copy"><i class="icon iconfont iconfuzhicopy22"></i></a></div>
+                <div class="time">{{getDayName(key.t)}} {{dateFormat(key.t)}}</div>
+              </div>
             </div>
-          </div>
-          <div class="actions">
-            <a class="del-colle" @click="deleteHistroyItem(key.t)">
-              <i class="icon iconfont iconshanchu"></i>
-            </a>
+            <div class="actions">
+              <a class="del-colle" @click="deleteHistroyItem(key.t)">
+                <i class="icon iconfont iconshanchu"></i>
+              </a>
+            </div>
           </div>
         </li>
         <li class="data-end"><i class="icon iconfont iconhuangguan"></i>没有更多了</li>
